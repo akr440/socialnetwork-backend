a8b57ef4ca6028a4e3135e00229418c33247b5a8
akr440 <anurag13012001@gmail.com>
2025-06-12 22:43:20 +0530

    add authentication feature

---
+++ accounts/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework.validators import ValidationError
+from rest_framework.authtoken.models import Token
+
+class SignupSerialsize(serializers.ModelSerializer):
+
+    email = serializers.EmailField(max_length=80)
+    username = serializers.CharField(max_length=45)
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email","password","username"]
+
+    def validate(self, attrs):
+            print(f"attrs value is {attrs}")
+            email_exists = User.objects.filter(email=attrs["email"]).exists()
+
+            if email_exists:
+                raise ValidationError("Email has already been used")
+
+            return super().validate(attrs)
+
+    def create(self,val):  #for hashing the password, automatically takes the password at the time of storing it 
+        #in the model and hash it before storing
+        password = val.pop("password")
+
+        user = super().create(val)
+
+        user.set_password(password)
+
+        user.save()
+        return user
---
+++ accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view,APIView
+from .import serializers
+from django.contrib.auth import authenticate
+from rest_framework.permissions import AllowAny
+# from rest_framework_simplejwt.tokens import RefreshToken
+# Create your views here.
+
+class UserSignup(APIView):
+
+    serializer_class=serializers.SignupSerialsize
+    def post(self,request:Request):
+        user_data=request.data
+        serialize=serializers.SignupSerialsize(data=user_data)
+        if serialize.is_valid():
+            serialize.save()
+            response={
+                "message":"user signed up succesfully",
+                "data":serialize.data
+            }
+            return Response(data=response,status=status.HTTP_201_CREATED)
+        return Response(data=serialize.error_messages,status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+    
+    def post(self,request:Request):
+        email=request.data.get("email")
+        password=request.data.get("password")
+        user=authenticate(email=email,password=password)
+        print(f"user is {user}")
+        if user is not None:
+            response = {"message": "Login Successfull", "user": request.user}
+            return Response(data=response, status=status.HTTP_200_OK)
+        else:
+            return Response(data={"message": "Invalid email or password"})
+
