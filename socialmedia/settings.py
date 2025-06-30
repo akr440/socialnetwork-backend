1b4757bca2fef7ef06cccf3151573871bfb405b4
akr440 <anurag13012001@gmail.com>
2025-06-30 14:54:29 +0530

    containerizing

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i-dqcv6j2wb(9)orr1ou0$k=0^kjp!xae5@)g-xj$lc+313w0h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-i-dqcv6j2wb(9)orr1ou0$k=0^kjp!xae5@)g-xj$lc+313w0h')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
@@ -87,8 +89,8 @@ ASGI_APPLICATION = 'socialmedia.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DJANGO_DATABASE_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': BASE_DIR / os.environ.get('DJANGO_DATABASE_NAME', 'db.sqlite3'),
     }
 }
 
